e2ab6ec2ddc8d39bfbc1fa08d2f9167a350b6858
Venkadesh <venkadesh.j@capestart.com>
2025-02-20 19:28:11 +0530

    day 2 tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Sample from './common_component.js';
+import {GettingProps} from  './common_component.js';
+import {Profile} from './common_component.js';
 
+function UserProfile({name,description}){
+ return <Profile  name={name} desc={description}/>;
+}
 function App() {
   return (
     <div className='d-flex'>
@@ -8,7 +13,11 @@ function App() {
       Hello World! &nbsp;
       </div>
      <Sample/>
-    </div>
+     <GettingProps sampleText = "Passing data from parent to child"/>
+     <UserProfile name="Venkat" description="React Developer"/>
+    </div>    
   );
 }
 export default App;
+
+
--- src/common_component.js
+++ src/common_component.js
@@ -1,8 +1,39 @@
 function sample() {
-return (
-    <div>
-   "Just started learning React!" 🚀
-    </div>
-);
+    return (
+        <div>
+            "Just started learning React!" 🚀
+        </div>
+    );
 }
- export default sample ;
+
+export default sample;
+
+function GettingProps(props) {
+    return (
+        <div className="sample-text">
+            {props.sampleText}
+        </div>
+    )
+}
+
+export { GettingProps };
+
+function GetImageUrl({imageUrl}) {
+    return <img className="react-img" src={imageUrl} alt="image url" />
+}
+
+function Profile({name,desc}){
+    return (
+        <div className="card">
+             <div className="profile-section">
+            <GetImageUrl imageUrl = "https://blog.logrocket.com/wp-content/uploads/2023/07/building-splash-screens-react-native.png"/>
+            <div className="profile-data">
+            <h4>{name}</h4>
+            <p>{desc}</p>
+            </div>
+        </div>
+        </div>
+       
+    )
+}
+export { Profile };
